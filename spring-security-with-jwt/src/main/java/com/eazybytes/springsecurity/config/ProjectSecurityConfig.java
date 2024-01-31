db80dd73bd8cd06258c6b2c1ab70e995b9095cd8
ujjwal gupta <ujjwal.kbr@gmail.com>
2024-01-31 17:32:28 +0530

    role based security

--- spring-security-with-jwt/src/main/java/com/eazybytes/springsecurity/config/ProjectSecurityConfig.java
+++ spring-security-with-jwt/src/main/java/com/eazybytes/springsecurity/config/ProjectSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -25,6 +26,7 @@ import java.util.Collections;
 
 
 @Configuration
+@EnableMethodSecurity(prePostEnabled = true,  securedEnabled = true,  jsr250Enabled = true)
 public class ProjectSecurityConfig {
 
     static class SecurityCorsConfiguration implements CorsConfigurationSource{
